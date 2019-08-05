3f57e720ed2bc5dc7ad84baf9e7d3b52bfbd225d
Mateusz Cieślakowski <cieslakowski.mat@gmail.com>
2019-08-05 19:07:05 +0200

    add

--- js/script.js
+++ js/script.js
@@ -27,39 +27,15 @@ var ctx = document.getElementById('myChart').getContext('2d');
 				        
 				    },
 				});
-/*
- type: 'bar',
-					    data: {
-					        labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
-					        datasets: [{
-					            label: '# of Votes',
-					            data: [12, 19, 3, 5, 2, 3],
-					            backgroundColor: [
-					                'rgba(255, 99, 132, 0.2)',
-					                'rgba(54, 162, 235, 0.2)',
-					                'rgba(255, 206, 86, 0.2)',
-					                'rgba(75, 192, 192, 0.2)',
-					                'rgba(153, 102, 255, 0.2)',
-					                'rgba(255, 159, 64, 0.2)'
-					            ],
-					            borderColor: [
-					                'rgba(255, 99, 132, 1)',
-					                'rgba(54, 162, 235, 1)',
-					                'rgba(255, 206, 86, 1)',
-					                'rgba(75, 192, 192, 1)',
-					                'rgba(153, 102, 255, 1)',
-					                'rgba(255, 159, 64, 1)'
-					            ],
-					            borderWidth: 0
-					        }]
-					    },
-					    options: {
-					        scales: {
-					            yAxes: [{
-					                ticks: {
-					                    beginAtZero: true
-					                }
-					            }]
-					        }
-					    }
-					});
+
+document.querySelector('form').addEventListener('submit', function(event) {
+  var isFormValidate = true;
+
+  var emailAddressInput = event.target.querySelector('input[name="email_address"]')
+  if(emailAddressInput.value.indexOf('@') < 0) {
+    isFormValidate = false;
+    emailAddressInput.parentElement.querySelector('.error').innerHTML = 'Błędny adres e-mail';
+  }
+
+  return !isFormValidate ? event.preventDefault() : true;
+})
