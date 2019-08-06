5c2e7caf7537b68ffaa21a3a2b1650433faef709
Mateusz Cieślakowski <cieslakowski.mat@gmail.com>
2019-08-06 02:56:05 +0200

    add and validate code

--- js/script.js
+++ js/script.js
@@ -25,17 +25,56 @@ var ctx = document.getElementById('myChart').getContext('2d');
 				            
 				        }]
 				        
-				    },
-				});
+	},
+});
 
-document.querySelector('form').addEventListener('submit', function(event) {
-  var isFormValidate = true;
+var menuButton = document.querySelector('.menuToggle');
+var navbar = document.querySelector('.navbar');
+var body = document.querySelector('body')
+menuButton.addEventListener('click', () => {
+	body.classList.toggle('hidden');
+	navbar.classList.toggle('hidden');
+});
 
-  var emailAddressInput = event.target.querySelector('input[name="email_address"]')
-  if(emailAddressInput.value.indexOf('@') < 0) {
-    isFormValidate = false;
-    emailAddressInput.parentElement.querySelector('.error').innerHTML = 'Błędny adres e-mail';
-  }
 
-  return !isFormValidate ? event.preventDefault() : true;
-})
+// var ctx = document.getElementById('myChart').getContext('2d');
+// 				var myChart = new Chart(ctx, {
+// 				// 1
+// 				    type: 'bar',
+// 				    data: {
+// 				        // 2
+// 				        labels: ["01", "02", "03", "04", "05", "06", "07", "08", "09", "10"],
+// 				        // 3
+// 				        datasets: [{
+// 				            // 4
+// 				            label: "Signups",
+// 				            // 5
+// 				            backgroundColor: '#8DBEC8',
+// 				            borderColor: '#8DBEC8',
+// 				            // 6
+// 				            data: [ 52, 51, 41, 94, 26, 6, 72, 9, 21, 88 ],
+// 				        },
+// 				        {
+// 				            label: "FTD",
+// 				            backgroundColor: '#F29E4E',
+// 				            borderColor: '#F29E4E',
+// 				            data: [ 6, 72, 1, 0, 47, 11, 50, 44, 63, 76 ],
+// 				        },
+// 				        {
+				            
+// 				        }]
+				        
+// 				    },
+// 				});
+
+// document.querySelector('form').addEventListener('submit', function(event) {
+//   var isFormValidate = true;
+
+//   var emailAddressInput = event.target.querySelector('input[name="email_address"]')
+//   if(emailAddressInput.value.indexOf('@') < 0) {
+//     isFormValidate = false;
+//     emailAddressInput.parentElement.querySelector('.error').innerHTML = 'Błędny adres e-mail';
+//   }
+
+//   return !isFormValidate ? event.preventDefault() : true;
+// })
